306eadc563a13dbb02dae64e4f3730a24891aa61
legion219 <legion.tg@gmail.com>
2017-04-01 09:45:13 +0100

    third try

---
+++ JS/CSS-Tricks-AnythingSlider-93cb5da/js/_vti_cnf/jquery.anythingslider.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|27 Nov 2016 18:44:43 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|JS/CSS-Tricks-AnythingSlider-93cb5da/video.html JS/CSS-Tricks-AnythingSlider-93cb5da/index.html JS/CSS-Tricks-AnythingSlider-93cb5da/css3.html JS/CSS-Tricks-AnythingSlider-93cb5da/demos.html JS/CSS-Tricks-AnythingSlider-93cb5da/expand.html JS/CSS-Tricks-AnythingSlider-93cb5da/simple.html
+vti_author:SR|MACHINE-PC\\machine
+vti_modifiedby:SR|MACHINE-PC\\machine
+vti_timecreated:TR|27 Nov 2016 18:44:43 -0000
+vti_cacheddtm:TX|27 Nov 2016 18:44:43 -0000
+vti_filesize:IR|43467
+vti_syncwith_localhost\\c\:\\users\\machine\\desktop\\scottish festival work in progress\\template\\master website templates/c\:/users/machine/desktop/scottish festival work in progress/template/master website templates:TR|27 Nov 2016 18:44:43 -0000
